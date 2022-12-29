4396cadb458d8c267a0aeab9c35881c220344a49
Tanvir Ahmed <61121499+TanvirAhmed1@users.noreply.github.com>
2022-12-29 13:07:37 +0600

    Identity Added

--- Aspnet Core Identity/Startup.cs
+++ Aspnet Core Identity/Startup.cs
@@ -1,6 +1,9 @@
+using Aspnet_Core_Identity.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -24,6 +27,8 @@ namespace Aspnet_Core_Identity
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
+            services.AddDbContext<AuthDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("AuthConnectionString")));
+            services.AddIdentity<IdentityUser, IdentityRole>().AddEntityFrameworkStores<AuthDbContext>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -44,6 +49,8 @@ namespace Aspnet_Core_Identity
             app.UseStaticFiles();
 
             app.UseRouting();
+
+            app.UseAuthentication();
 
             app.UseAuthorization();
 
---
+++ Aspnet Core Identity/ViewModels/Register.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Aspnet_Core_Identity.ViewModels
+{
+    public class Register
+    {
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        [Compare(nameof(Password), ErrorMessage ="Password and Confirmation Password Did Not Match")]
+        public string ConfirmPassword { get; set; }
+    }
+}
